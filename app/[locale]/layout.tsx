94c05e5962ab3dde93b199c46e49607572a35497
Grizzly <gabilomtatidze@gmail.com>
2026-04-06 23:01:39 +0400

    update routing and middleware

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -59,7 +59,7 @@ export default async function RootLayout({ children, params }: LayoutProps<'/[lo
   const { locale } = await params;
 
   return (
-    <html lang={locale}>
+    <html lang={locale} data-scroll-behavior="smooth">
       <body className="min-h-full flex flex-col bg-[#0F0F0F]">
         <NextIntlClientProvider locale={locale}>
           {children}
--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,19 @@
-// import createMiddleware from 'next-intl/middleware';
-// import { routing } from './i18n/routing';
-
-// export default createMiddleware(routing);
-
-// export const config = {
-//   matcher: ['/((?!api|_next|.*\\..*).*)']
-// };
-
 import createMiddleware from 'next-intl/middleware';
+import { routing } from './i18n/routing';
 
-export default createMiddleware({
-  locales: ['en', 'ka'],
-  defaultLocale: 'en'
-});
+export default createMiddleware(routing);
 
 export const config = {
-  matcher: ['/', '/(en|ka)/:path*']
+  matcher: ['/((?!api|_next|.*\\..*).*)']
 };
+
+// import createMiddleware from 'next-intl/middleware';
+
+// export default createMiddleware({
+//   locales: ['en', 'ka'],
+//   defaultLocale: 'en'
+// });
+
+// export const config = {
+//   matcher: ['/', '/(en|ka)/:path*']
+// };

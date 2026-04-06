76ba26329a51a4b78d6c285899fd44fc2d2d1ece
Grizzly <gabilomtatidze@gmail.com>
2026-04-07 02:02:09 +0400

    test middleware

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,9 +1,7 @@
 "use client";
 import Header from "../../components/header";
 import Footer from "../../components/footer";
-import { useEffect } from "react";
 import Main from "./main/page";
-import { useUserStore } from "@/zustand/User/profileStore";
 
 
 // wogofaj349@fftube.com
